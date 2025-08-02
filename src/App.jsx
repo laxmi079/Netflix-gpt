5a4e1c118c70a2a4cba4964a4b96224dd6951d72
laxmi0799 <laxmi.kumari.mec22@itbhu.ac.in>
2025-08-02 22:42:37 +0530

    initial files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const App=()=>{
+  return (
+     <div className="bg-amber-400">
+        This is my start of project
+     </div>
+  )
+
+}
+
+export default App
